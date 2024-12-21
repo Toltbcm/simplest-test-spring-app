b62fc206d9f086de4ebde24e344ade8002c11226
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-12-21 06:09:15 +0200

    add application.properties

--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -3,24 +3,32 @@ package org.example.config;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.hibernate.cfg.Environment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 
 @Configuration
 @ComponentScan(basePackages = "org.example")
+@PropertySource("classpath:application.properties")
 public class AppConfig {
+    private Environment environment;
+
+    public AppConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     private DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
 
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5433/test_db");
-        dataSource.setDefaultSchema("test_schema");
-        dataSource.setUsername("test_user");
-        dataSource.setPassword("test_password");
+        dataSource.setDriverClassName(environment.getProperty("db.driver"));
+        dataSource.setUrl(environment.getProperty("db.url"));
+        dataSource.setDefaultSchema(environment.getProperty("db.schema"));
+        dataSource.setUsername(environment.getProperty("db.username"));
+        dataSource.setPassword(environment.getProperty("db.password"));
 
         return dataSource;
     }
@@ -31,11 +39,16 @@ public class AppConfig {
         localSessionFactoryBean.setDataSource(getDataSource());
 
         Properties properties = new Properties();
-        properties.put(Environment.SHOW_SQL, true);
-        properties.put(Environment.HBM2DDL_AUTO, "validate");
-        properties.put(Environment.FORMAT_SQL, false);
-        properties.put(Environment.USE_SQL_COMMENTS, true);
-        properties.put(Environment.HIKARI_MAX_SIZE, 10);
+        properties.put(org.hibernate.cfg.Environment.SHOW_SQL,
+                environment.getProperty("hibernate.show_sql"));
+        properties.put(org.hibernate.cfg.Environment.FORMAT_SQL,
+                environment.getProperty("hibernate.format_sql"));
+        properties.put(org.hibernate.cfg.Environment.USE_SQL_COMMENTS,
+                environment.getProperty("hibernate.use_sql_comments"));
+        properties.put(org.hibernate.cfg.Environment.HIKARI_MAX_SIZE,
+                environment.getProperty("hibernate.maximumPoolSize"));
+        properties.put(org.hibernate.cfg.Environment.HBM2DDL_AUTO,
+                environment.getProperty("hibernate.hbm2ddl.auto"));
 
         localSessionFactoryBean.setHibernateProperties(properties);
         localSessionFactoryBean.setPackagesToScan("org.example.model");
